555b017363f8ffa978977ccdcf8a65d5acc4a78f
Yoshikawa Teru <teruyamato0731@gmail.com>
2023-08-12 18:46:20 +0900

    feat: Amt21のメッセージをデータに変換

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,34 @@ BufferedSerial pc{USBTX, USBRX, 115200};
 Timer timer;
 
 // Struct definition
+struct Amt21_C {
+  static constexpr int rotate = 4096;
+
+  uint8_t address;
+  uint16_t pos;
+  uint16_t turns;
+
+  bool read_pos(const uint16_t msg) {
+    bool res = is_valid(msg);
+    if(res) pos = (msg & 0x3fff) >> 2;
+    return res;
+  }
+  bool read_turns(const uint16_t msg) {
+    bool res = is_valid(msg);
+    if(res) turns = msg & 0x3fff;
+    return res;
+  }
+  static bool is_valid(uint16_t raw_data) {
+    bool k1 = raw_data >> 15;
+    bool k0 = raw_data >> 14 & 1;
+    raw_data <<= 2;
+    do {
+      k1 ^= raw_data & 0x8000;          // even
+      k0 ^= (raw_data <<= 1) & 0x8000;  // odd
+    } while(raw_data <<= 1);
+    return k0 && k1;
+  }
+};
 
 // Global variable
 
