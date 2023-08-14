c7aab6d0ecb4b64632ef30d59f0de0b3b4bd0410
Yoshikawa Teru <teruyamato0731@gmail.com>
2023-08-14 21:36:20 +0900

    feat: Rs485をクラス化

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,15 @@
 #include <mbed.h>
+
 #include "Amt21.h"
+#include "Rs485.h"
 
 // Const variable
 
 // Function prototype
-void uart_transmit(const uint8_t *send, const int len);
-template<int N>
-void uart_transmit(const uint8_t (&send)[N]);
-bool uart_receive(void *buf, const int len, const std::chrono::milliseconds timeout);
-template<int N>
-bool uart_receive(uint8_t (&buf)[N], const std::chrono::milliseconds timeout);
 
 // IO
 BufferedSerial pc{USBTX, USBRX, 115200};
-// 2Mbps-8-N-1
-BufferedSerial enc_bus{PB_6, PA_10, (int)2e6};
-DigitalOut de{PC_0};
+Rs485 rs485{PB_6, PA_10, (int)2e6, PC_0};
 // CAN can{PA_11, PA_12, (int)1e6};
 // CAN can{PB_12, PB_13, (int)1e6};
 // CANMessage msg;
@@ -27,23 +21,23 @@ Timer timer;
 struct : Amt21 {
   // request
   bool send_read_pos() {
-    uart_transmit({address});
-    if(uint16_t receive; uart_receive(&receive, sizeof(receive), 10ms)) {
+    rs485.uart_transmit({address});
+    if(uint16_t receive; rs485.uart_receive(&receive, sizeof(receive), 10ms)) {
       read_pos(receive);
       return true;
     }
     return false;
   }
   bool send_read_turns() {
-    uart_transmit({address + 1});
-    if(uint16_t receive; uart_receive(&receive, sizeof(receive), 10ms)) {
+    rs485.uart_transmit({address + 1});
+    if(uint16_t receive; rs485.uart_receive(&receive, sizeof(receive), 10ms)) {
       read_turns(receive);
       return true;
     }
     return false;
   }
   void send_reset() {
-    uart_transmit({address + 2, 0x75});
+    rs485.uart_transmit({address + 2, 0x75});
   }
 } amt{0x54};
 
@@ -53,7 +47,6 @@ int main() {
   printf("\nsetup\n");
   timer.start();
   auto pre = timer.elapsed_time();
-  enc_bus.set_blocking(0);
   amt.send_reset();
   while(1) {
     // put your main code here, to run repeatedly:
@@ -71,31 +64,3 @@ int main() {
 }
 
 // Function definition
-void uart_transmit(const uint8_t *send, const int len) {
-  de = 1;
-  enc_bus.sync();
-  enc_bus.write(send, len);
-  wait_us(3);
-  de = 0;
-}
-template<int N>
-void uart_transmit(const uint8_t (&send)[N]) {
-  uart_transmit(send, sizeof(send));
-}
-bool uart_receive(void *buf, const int len, const std::chrono::milliseconds timeout) {
-  auto now = timer.elapsed_time();
-  auto pre = now;
-  uint8_t *p = reinterpret_cast<uint8_t *>(buf);
-  const uint8_t *end = p + len;
-  do {
-    if(enc_bus.read(p, 1) > 0 && ++p == end) {
-      return (wait_ns(275), true);
-    }
-    now = timer.elapsed_time();
-  } while(now - pre < timeout);
-  return false;
-}
-template<int N>
-bool uart_receive(uint8_t (&buf)[N], const std::chrono::milliseconds timeout) {
-  return uart_receive(buf, sizeof(buf), timeout);
-}
